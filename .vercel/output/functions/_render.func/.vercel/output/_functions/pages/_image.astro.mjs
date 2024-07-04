15040b1f9ad6598a4042e655c2e1f6721d4a1c19
MatecEDS <matec.eds@gmail.com>
2024-07-04 09:47:48 -0600

    version 1.0.2

---
+++ .vercel/output/functions/_render.func/.vercel/output/_functions/pages/_image.astro.mjs
@@ -0,0 +1,5 @@
+export { renderers } from '../renderers.mjs';
+
+const page = () => import('../chunks/generic_DdstFm6L.mjs');
+
+export { page };
---
+++ .vercel/output/functions/_render.func/.vercel/output/_functions/pages/index.astro.mjs
@@ -0,0 +1,5 @@
+export { renderers } from '../renderers.mjs';
+
+const page = () => import('../chunks/index_rHPGny97.mjs');
+
+export { page };
